545e293ea5eb5728566c7aa65ec038f522d33150
BT_1n3_22 <sample@bths.edu>
2023-09-22 09:28:31 -0400

    Student class

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    // instance
+    private String name;
+    private int grade;
+    private double gpa;
+
+    public Student(String StudentName, int StudentGrade, double StudentGPA) {
+        name = StudentName;
+        grade = StudentGrade;
+        double = StudentGPA;
+    }
+    public introduce() {
+        System.out.println("Hello my name is " + name);
+    }
+
+}
