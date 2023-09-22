0746fb3c1b049f1777675ea8906c0b06a67e8a24
BT_1n3_22 <sample@bths.edu>
2023-09-22 09:07:27 -0400

    Cat start

---
+++ src/CatRunner.java
@@ -0,0 +1,7 @@
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Lovelle", 6, 15);
+        cat1.introduce();
+        cat1.printCatInfo();
+    }
+}
--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+        // creating one instance/object from the Rectangle class
+        Rectangle rect1 = new Rectangle(5, 6);
+        rect1.printArea();  // calling a method on the object
+
+        // creating ANOTHER instance/object from the Rectangle class
+        Rectangle rect2 = new Rectangle(10, 8);
+        rect2.printArea();  // calling a method on the object
+
+        Rectangle rect3 = new Rectangle(26, 13);
+        rect3.printArea();
+    }
 }
+
+
