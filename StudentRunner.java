15fc1eb6ff20b55af57de83327c1567416c98fc1
Shifatulg <145681447+Shifatulg@users.noreply.github.com>
2023-09-24 17:58:34 -0400

    StudentRunner final

---
+++ StudentRunner.java
@@ -0,0 +1,8 @@
+public class StudentRunner {
+  public static void main(String[] args) {
+    student student1 = new student("Jerry", 11, 3.6);
+      student1.introduce();
+    student student2 = new student("Henry", 6, 4.0);
+      student2.introduce(); 
+  }
+}
